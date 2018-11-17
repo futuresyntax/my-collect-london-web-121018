3b06154ac0a58403a43deb64c04a71ab2300034a
FutureScript <fernandobaxter1@gmail.com>
2018-11-17 21:37:34 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,13 @@
 
-def my_collect(array) # put argument(s) here
-  # code here
-  i = 0
-  result = []
 
-  while i < array.size
-    block_given?
-    result << yield(array[i])
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
     i += 1
   end
-result
+  new_collection
 end
 
 #not my code, on the 17th of november instructor told me not to spend too much time on understanding yield because it's not used too much.
